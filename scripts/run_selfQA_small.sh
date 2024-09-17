24f961d6c5acaa7ff3867ddb50824bfb4b1c6bf7
Zachary Sun <sunguohao8895@gmail.com>
2024-09-17 14:27:40 -0400

    Create run_selfQA_small.sh

---
+++ scripts/run_selfQA_small.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+CUDA_VISIBLE_DEVICES=0,1
+
+gpu_list="${CUDA_VISIBLE_DEVICES:-0}"
+IFS=',' read -ra GPULIST <<< "$gpu_list"
+
+CHUNKS=${#GPULIST[@]}
+
+CKPT="sqllava-ov-0.5b-100k"
+for IDX in $(seq 0 $((CHUNKS-1))); do
+    CUDA_VISIBLE_DEVICES=${GPULIST[$IDX]} python -m llava.eval.visual_qa \
+        --model-path ./checkpoints/sqllava-lora-qwen-0.5b-ovVideo \
+        --model-base lmms-lab/llava-onevision-qwen2-0.5b-ov \
+        --question-file ./data/m4_instruct_video.json \
+        --video-folder ./data/video \
+        --answers-file ${CHUNKS}_${IDX}.json \
+        --out_dir ./data/selfqa/$CKPT \
+        --num-chunks $CHUNKS \
+        --chunk-idx $IDX \
+        --test_size 1000000 &
+done
+
+wait
+
+output_file=./data/selfqa/$CKPT/merge.json
+
+# Clear out the output file if it exists.
+> "$output_file"
+
+# Loop through the indices and concatenate each file.
+for IDX in $(seq 0 $((CHUNKS-1))); do
+    cat ./data/selfqa/$CKPT/${CHUNKS}_${IDX}.json >> "$output_file"
+done
+
+
+
+
