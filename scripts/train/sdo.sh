0039ce1c462c28a8583a73cfc3be25c20a2338b1
Zachary Sun <sunguohao8895@gmail.com>
2024-10-29 22:13:37 -0400

    update training script

--- scripts/train/sdo.sh
+++ scripts/train/sdo.sh
@@ -1,15 +1,15 @@
 ACCELERATE_CPU_AFFINITY=1 torchrun --nproc_per_node=8 --nnodes=1  \
     llava/train/train_sdo.py \
-    --lora_enable True --lora_r 64 --lora_alpha 128 --mm_projector_lr 1e-6 \
+    --lora_enable True --lora_r 64 --lora_alpha 128 --mm_projector_lr 1e-7 \
     --deepspeed scripts/zero3.json \
     --model_name_or_path ZachSun/ours-qwen-7b-interleave \
     --version qwen_1_5\
-    --sdo_alpha_a 1.0 --sdo_alpha_q 1.0 --beta 0.1 --gamma 0 --lamda 50\
+    --sdo_alpha_a 1.2 --sdo_alpha_q 1.0 --beta 0.1 --gamma 0 --lamda 50\
     --data_path ./data/labling/7b-sqa-labling/merge_prefQA_7B.json \
     --image_folder ./data/image \
     --video_folder ./data/video \
     --mm_tunable_parts="mm_vision_tower,mm_mlp_adapter,mm_language_model" \
-    --mm_vision_tower_lr 5e-6 \
+    --mm_vision_tower_lr 5e-7 \
     --vit_lora_enable \
     --lora_alpha_vit 64 \
     --lora_r_vit 32 \
@@ -23,9 +23,9 @@ ACCELERATE_CPU_AFFINITY=1 torchrun --nproc_per_node=8 --nnodes=1  \
     --image_grid_pinpoints  "(1x1),...,(6x6)" \
     --mm_patch_merge_type spatial_unpad \
     --bf16 True \
-    --run_name ours-7b-qwen-lora-sdo-g0-lr1e5-lmd50-2epo \
-    --output_dir "./checkpoints/ours-7b-qwen-lora-sdo-g0-lr1e5-lmd50-2epo-newPrefv2" \
-    --num_train_epochs 2 \
+    --run_name ours-7b-qwen-lora-sdo-lr5e7-lmd50-3epo \
+    --output_dir "./checkpoints/ours-7b-qwen-lora-sdo-lr5e7-lmd50-3epo-newPrefv2" \
+    --num_train_epochs 3 \
     --per_device_train_batch_size 1 \
     --per_device_eval_batch_size 4 \
     --gradient_accumulation_steps 16 \
@@ -33,7 +33,7 @@ ACCELERATE_CPU_AFFINITY=1 torchrun --nproc_per_node=8 --nnodes=1  \
     --save_strategy "steps" \
     --save_steps 1000000 \
     --save_total_limit 1 \
-    --learning_rate 5e-6 \
+    --learning_rate 5e-7 \
     --weight_decay 0. \
     --warmup_ratio 0.1 \
     --lr_scheduler_type "linear" \
