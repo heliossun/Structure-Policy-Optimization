ddb4e3be690e2a45e422ff937ee6b5fbb7a6fd3d
Zachary Sun <sunguohao8895@gmail.com>
2025-10-18 21:26:21 -0700

    iccv

--- scripts/merge_lora.sh
+++
@@ -1,7 +0,0 @@
-
-
-python -m scripts.merge_lora_base \
-    --model-path ./checkpoints/sqllava-7b-qwen-lora-spo-Q-b0.2-lmd10 \
-	--model-base ZachSun/sqllava-qwen-7b-interleave \
-    --model_name sqllava_qwen-lora-7b \
-    --output_dir ./checkpoints/sqllava-qwen-7b-lora-spo-Q-merge
